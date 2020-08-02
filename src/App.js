a18c85bdaa4a53c7242da1e8fd8ee40cd5903524
= <=>
2020-08-02 17:45:51 +0530

    collection preview & collection item addded

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import { Route } from 'react-router-dom'
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component'
 
 function App() {
   return (
-    <HomePage />
+    <div>
+      <Route exact = 'true' path = '/' component = { HomePage } />
+      <Route exact = 'true' path = '/shop' component = { ShopPage } />
+     </div>
   );
 }
 
